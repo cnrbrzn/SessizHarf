f6b7325a4dfc8cfa23cce434ab1f977e1784460b
Caner Borazan <caner.borazan@lunatr.com>
2022-11-15 15:47:35 +0300

    Add project files.

---
+++ SessizHarf/Program.cs
@@ -0,0 +1,23 @@
+﻿//Algoritma
+//Verilen string ifade içerisinde yanyana 2 tane sessiz varsa ekrana true, yoksa false yazdıran console uygulamasını yazınız.
+
+//Örnek: Input: Merhaba Umut Arya
+
+//Output: True False True
+string ent = Console.ReadLine();
+string[] str = ent.Split(' ');
+char[] vovels = { 'a', 'e', 'ı', 'i', 'o', 'ö', 'u', 'ü', 'A', 'E', 'I', 'İ', 'O', 'Ö', 'U', 'Ü' };
+for (int i = 0; i < str.Length; i++)
+{
+    bool value = false;
+    char[] chars = str[i].ToCharArray();
+    for (int j = 0; j < chars.Length; j++)
+    {
+        if (!vovels.Contains(chars[j]) && j != chars.Length - 1 && !vovels.Contains(chars[j + 1]))
+        {
+            value = true;
+            break;
+        }
+    }
+    Console.Write(value + " ");
+}
